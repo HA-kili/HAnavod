0908cd22111065f3aa262a65504aac74cef0285e
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-03-30 14:11:32 +0200

    Add files via upload

--- menu/home.php
+++ menu/home.php
@@ -14,11 +14,12 @@
                         <a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=722006"><button class="nick"> DementedMate | NAT </button></a> &nbsp; - technická časť version 1.01
                         <br><br>
                         <a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=37083"><button class="nick"> kili | HLP </button></a> &nbsp; - technická časť version 2.01 + obsahová časť 
-                        <br><br><br><br>
-                        <b><u> Poďakovanie patrí aj manažérovi : </u></b>
                         <br><br>
-                        <a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=227387"><button class="nick"> airen </button></a> &nbsp; - rady + <a href="./index.php?navod=doplnky_(addons)"> doplnky (addons) </a>
+                        <a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=227387"><button class="nick"> airen </button></a> &nbsp; - rady + <a href="./index.php?navod=doplnky_(addons)"> doplnky pre HA (addons) </a>
+                        <br><br> 
+                        <b><h5 class="red" style="line-height: 1.6;"> Translator addons for <a href="https://addons.mozilla.org/sk/firefox/addon/to-google-translate/">firefox</a> , <a href="https://chrome.google.com/webstore/detail/google-translate/aapbdbdomjkkjkaonfhkkikfgjllcleb/related?hl=sk">chrome</a> and <a href="https://addons.opera.com/en/extensions/details/translator/">opera</a> to change the language of the hockeyarena manual.  </h5></b>      
                     </div>
+                    
 
                 <style>
                     <?php include ('./css/pismo10.css');?>
