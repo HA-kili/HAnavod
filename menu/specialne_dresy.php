fcf6195475a6010f00542e6d3e897c5b31ee57fc
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-04-10 11:10:22 +0200

    Add files via upload

--- menu/specialne_dresy.php
+++ menu/specialne_dresy.php
@@ -8,87 +8,87 @@
                     Bez SB je tieto zmeny možné robiť cez Menu - <a href="https://www.hockeyarena.net/sk/index.php?p=manager_jersey.inc"> Hráči - Dres </a> ,  
                     kde sa nachádza zoznam všetkých hráčov, ktorý vlastnia dres. Po kliknutí na hráča sa ukážu dresy, kde je následne možné robiť zmeny.
                     <br><br><br>
-                    <u> 28.03.2022 - Apríl: Jarný odmäk </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=14009"><u> 28.03.2022 - Apríl: Jarný odmäk </u></a>
                     <br> Bonus : jednorázové zvýšenie streľby o 10
                     <br> Pri deaktivácii : zodpovedajúce zníženie streľby 
                     <br><br>
-                    <u> 04.02.2022 - Február: Valentínsky Február </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13949"><u> 04.02.2022 - Február: Valentínsky Február </u></a>
                     <br> Bonus : zvýšenie sebaovládania +10
                     <br> Pri deaktivácii : zodpovedajúce zníženie sebaovládania
                     <br><br>
-                    <u> 09.12.2021 - December : Veselé sviatky </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13920"><u> 09.12.2021 - December : Veselé sviatky </u></a>
                     <br> Bonus: zvýšenie potenciálu hráča o jeden level 
                     <br> Pri deaktivácii : zodpovedajúce zníženie potenciálu hráča o jeden level
                     <br><br>
-                    <u> 03.11.2021 - November: Legendy </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13807"><u> 03.11.2021 - November: Legendy </u></a>
                     <br> Bonus: zvýšenie nahrávky +15, aktuálne zranenie -1 deň
                     <br> Pri deaktivácii : zodpovedajúce zníženie nahrávky
                     <br><br>
-                    <u> 06.10.2021 - Október: Halloween kúzla/maškrty </u> 
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13720"><u> 06.10.2021 - Október: Halloween kúzla/maškrty </u></a>
                     <br> Bonus: zvýšenie sebaovládania (rýchlosti) o 10 bodov
                     <br> Pri deaktivácii : zodpovedajúce zníženie atribútu
                     <br><br>
-                    <u> 03.09.2021 - September: Šťastný služobník </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13661"><u> 03.09.2021 - September: Šťastný služobník </u></a>
                     <br> Bonus: Lojalita +3 (zvýši o 3 stupne), skúsenosť -80, nedá sa aktivovať, ak je hráč na trhu
                     <br> Pri deaktivácii : zodpovedajúce zníženie lojality a zvýšenie skúsenosti
                     <br><br>
-                    <u> 05.08.2021 – August: Streľba na terč </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13605"><u> 05.08.2021 – August: Streľba na terč </u></a>
                     <br> Bonus: Brána +10 
                     <br> Pri deaktivácii : zodpovedajúce zníženie brány
                     <br><br>    
-                    <u> 02.07.2021 – Júl: Letná dovolenka </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13547"><u> 02.07.2021 – Júl: Letná dovolenka </u></a>
                     <br> Bonus: Sila +10 
                     <br> Pri deaktivácii : zodpovedajúce zníženie sily
                     <br><br>   
-                    <u> 02.06.2021 – Jún: Zotavenie </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13462"><u> 02.06.2021 – Jún: Zotavenie </u></a>
                     <br> Bonus: Pri aktivovaní tohto dresu sa skráti aktuálne zranenie hráča o 2 dni
                     <br> Pri deaktivácii : sa nestane nič, hráčovi sa nevráti zranenie spať 
                     <br><br>
-                    <u> 01.05.2021 - Playoff spása </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13349"><u> 01.05.2021 - Playoff spása </u></a>
                     <br> Bonus: jednorázové zvýšenie útoku o 10 
                     <br> Pri deaktivácii: zodpovedajúce zníženie útoku 
                     <br><br>
-                    <u> 03.04.2021 - Apríl: Jarný odmäk </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13265"><u> 03.04.2021 - Apríl: Jarný odmäk </u></a>
                     <br> Bonus: jednorázové zvýšenie streľby o 10 
                     <br> Pri deaktivácii: zodpovedajúce zníženie streľby 
                     <br><br>
-                    <u> 05.03.2021 - Marec: Vyformuj sa </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13235"><u> 05.03.2021 - Marec: Vyformuj sa </u></a>
                     <br> Bonus: jednorázové zvýšenie aktuálnej formy o 40 
                     <br> Pri deaktivácii: jednorázové zníženie aktuálnej formy o 40 
                     <br><br>
-                    <u> 03.02.2021 - Február: Šťastný Valentín </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13089"><u> 03.02.2021 - Február: Šťastný Valentín </u></a>
                     <br> Bonus: zníženie platu hráča o 20% 
                     <br> Pri deaktivácii: zodpovedajúce zvýšenie platu 
                     <br><br>
-                    <u> 01.01.2021 - Január: Výber hviezd </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13033"><u> 01.01.2021 - Január: Výber hviezd </u></a>
                     <br> Bonus pri aktivácii: zvýšenie hviezd hráča o jednu 
                     <br> Pri deaktivácii: zodpovedajúce zníženie hviezd hráča 
                     <br><br>
-                    <u> 01.12.2020 - December: Veselé sviatky! </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13002"><u> 01.12.2020 - December: Veselé sviatky! </u></a>
                     <br> Bonus: zvýšenie potenciálu hráča o jeden level 
                     <br> Pri deaktivácii : zodpovedajúce zníženie potenciálu hráča o jeden level
                     <br><br>
-                    <u> 03.11.2020 - November: Hokejové legendy </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=12943"><u> 03.11.2020 - November: Hokejové legendy </u></a>
                     <br> Bonus: nahrávka +10, aktuálne zranenie -1 deň
                     <br> Pri deaktivácii : zodpovedajúce zniženie nahrávky, zranenie sa hráčovi nevráti
                     <br><br>
-                    <u> 02.10.2020 - Október: Halloween </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=12827"><u> 02.10.2020 - Október: Halloween </u></a>
                     <br> Bonus: sebaovládanie +10
                     <br> Pri deaktivácii : zodpovedné zníženie sebaovládania
                     <br><br>
-                    <u> 03.09.2020 - September - Šťastný služobník </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=12799"><u> 03.09.2020 - September - Šťastný služobník </u></a>
                     <br> Bonus: Lojalita +3 (zvýši o 3 stupne), skúsenosť -30, nedá sa aktivovať, ak je hráč na trhu
                     <br> Pri deaktivácii : zodpovedné zníženie lojality a zvýšenie skúsenosti
                     <br><br>
-                    <u> 28.07.2020 - August: Streľba na terč </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=12741"><u> 28.07.2020 - August: Streľba na terč </u></a>
                     <br> Bonus: Brána +10 
                     <br> Pri deaktivácii : zodpovedné zníženie brány
                     <br><br>
-                    <u> 01.07.2020 - Júl: Letná dovolenka </u> 
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=12685"><u> 01.07.2020 - Júl: Letná dovolenka </u></a>
                     <br> Bonus: Sila +10 
                     <br> Pri deaktivácii : zodpovedné zníženie sily
                     <br><br>
-                    <u> 18.06.2020 - Jún: Zotavenie </u>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=12657"><u> 18.06.2020 - Jún: Zotavenie </u></a>
                     <br> Bonus: Pri aktivovaní tohto dresu sa skráti aktuálne zranenie hráča o 2 dni
                     <br> Pri deaktivácii : sa nestane nič, hráčovi sa nevráti zranenie spať 
                     <br><br> <br>
