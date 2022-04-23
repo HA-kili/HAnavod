a4b7a9795e59d05767eb777119935e84c23082f3
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-04-23 23:01:26 +0200

    Add files via upload

--- menu/informacie_muzstve.php
+++ menu/informacie_muzstve.php
@@ -1,15 +1,13 @@
 
                 <div class="section-name"> Detailné informácie o mužstve </div>
                     <br>
-                    <ul>
-                        <li><b><h1> ID mužstva </h1></b> - ID mužstva v databáze hry </li>
-                        <br>
-                        <li><b><h1> Manažér </h1></b> - meno manažéra mužstva, po <a href="https://www.hockeyarena.net/sk/index.php?p=manager_summary.php">kliknutí</a> sa zobrazí detailná informácia o manažérovi </li>
-                        <br>
-                        <li><b><h1> Meno tímu </h1></b> - je možné ho zmeniť raz za sezónu v menu <a href="https://www.hockeyarena.net/sk/index.php?p=manager_settings_form.php">"Manažér - Nastavenie"</a> </li>
-                        <br>
-                        <li><b><h1> Liga </h1></b> - číslo a skupina <a href="https://www.hockeyarena.net/sk/index.php?p=public_standings.inc">ligy</a> , v ktorej mužstvo hrá </li>
-                        <br>
-                        <li><b><h1> Založený </h1></b> - dátum založenia mužstva, mení sa pri vzniku a reštartovaní mužstva </li>
-                    </ul>
+                        <b><h1> ID mužstva </h1></b> - ID mužstva v databáze hry
+                        <br><br>
+                        <b><h1> Manažér </h1></b> - meno manažéra mužstva, po <a href="https://www.hockeyarena.net/sk/index.php?p=manager_summary.php">kliknutí</a> sa zobrazí detailná informácia o manažérovi 
+                        <br><br>
+                        <b><h1> Meno tímu </h1></b> - je možné ho zmeniť raz za sezónu v menu <a href="https://www.hockeyarena.net/sk/index.php?p=manager_settings_form.php">"Manažér - Nastavenie"</a> 
+                        <br><br>
+                        <b><h1> Liga </h1></b> - číslo a skupina <a href="https://www.hockeyarena.net/sk/index.php?p=public_standings.inc">ligy</a> , v ktorej mužstvo hrá 
+                        <br><br>
+                        <b><h1> Založený </h1></b> - dátum založenia mužstva, mení sa pri vzniku a reštartovaní mužstva 
                     <br><br>
