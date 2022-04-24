9212c9c9b3e299bf016dd8f26c2fe1b878328cf4
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-04-24 20:39:41 +0200

    Add files via upload

--- menu/trening.php
+++ menu/trening.php
@@ -8,13 +8,23 @@
                     <br> - priradenie jednotlivých režimov k hráčom v menu <a href="https://www.hockeyarena.net/sk/index.php?p=manager_training_form1.php">Hráči - Tréning</a> 
                     <br><br> 
                     V okne <a href="https://www.hockeyarena.net/sk/index.php?p=manager_training_form1.php">Tréning</a>  sa zobrazujú hráči, ich atribúty a pokrok v tréningu
-                    za predchádzajúci deň (Tré). 
+                    za predchádzajúci deň (Tré). Pohyb atribútu znázorňuje aj zelená šípky (nárast) alebo červená šípka (pokles). 
                     <br><br> 
                     Zvolením tréningového režimu v riadku hráča je možné nastaviť tréning alebo voľno pre jednotlivých hráčov. 
                     <br> 
-                    Zmeny v nastavení treba potvrdiť tlačidlom Potvrď. 
+                    Zmeny v nastavení treba potvrdiť tlačidlom "Potvrď" . 
                     <br><br> 
                     Výška atribútu závisí od kvality hráča. Totižto hráč si vie vytrénovať atribút max. do výšky 2,5* násobku kvality. Potom sa tréning daného atribútu výrazne spomalí.
                     <br><br>
-                    <b> Maximálna výška každého atribútu je 255. Prípadné aktivovanie dresu, ktorý má zvýšiť atribút hráča o 10 bodov, sa tento atribut aj napriek tomu nenavýši. </b>
+                    <b> Maximálna výška každého atribútu je 255. Prípadné aktivovanie dresu, ktorý má zvýšiť atribút hráča o 10 bodov, sa tento atribút aj napriek tomu nenavýši. </b>
+                    <br><br><br><br>
+                    <i><u> A ako trénovať jednotlivé atribúty hráčov? </u>
                     <br><br>
+                    Hráči sa trénujú podľa toho, aké <a href="index.php?navod=takticke_specializacie"> taktické špecializácie formácií</a> hrávate. 
+                    <br><br>
+                    Príklad : 
+                    <br>
+                    Zvolená taktika formácie bude "Protiútoky". Čiže taktické profily hráčov na krídle sú Rýchly (RÝC), na centri Tvorca (TVO) alebo Nahrávač (NAH) a u obrancov je to Nahrávač (NAH) 
+                    alebo čistý (ČIS). V ďalšej tabuľke nižšie už vidíme ako tieto taktické profily hráčov trénovať. Samozrejme toto sú hlavné atribúty a okrem nich treba hráčom dotrénovať aj 
+                    vedľajšie atribúty ako napr. sebaovládanie hráčov kvoli vylučovaniu. Popis jednotlivých atribútov nájdete <a href="index.php?navod=zakladne_atributy"> tu</a> .
+                    </i>
