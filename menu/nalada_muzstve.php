046ce7fd6717db0150b7332c55dd9712b75637af
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-05-10 18:28:22 +0200

    Add files via upload

--- menu/nalada_muzstve.php
+++ menu/nalada_muzstve.php
@@ -4,10 +4,10 @@
                     Nálada mužstva je jeden z faktorov, ktorý určuje <a href="https://www.hockeyarena.net/sk/index.php?p=manager_scouting_form.php">motiváciu</a> hráčov na zápase (motivíciu je možné vidieť v oddelení prieskumu).  Nálada sa mení sa v týchto prípadoch : 
                     <br>
                     <ul>
-                        <li> ligový zápas, zápasy turnajov Svetová liga, Národný pohár a Pohár národných šampiónov s nízkou dôležitosťou - zlepšenie </li>
-                        <li> ligový zápas, zápasy turnajov Svetová liga, Národný pohár a Pohár národných šampiónov s vysokou dôležitosťou - zhoršenie </li>
-                        <li> predaj hráča, ktorý nie je v mužstve aspon 2 týždne – výrazné zhoršenie (neplatí, ak je ten hráč čerstvý junior) </li>
-                        <li> predaj hráča, ktorý je v mužstve 20 týždňov a viac – mierne zhoršenie </li>
+                        <li> ligový zápas, zápasy turnajov Svetová liga, Národný pohár a Pohár národných šampiónov s nízkou dôležitosťou - <b> zlepšenie </b></li>
+                        <li> ligový zápas, zápasy turnajov Svetová liga, Národný pohár a Pohár národných šampiónov s vysokou dôležitosťou - <b> zhoršenie </b></li>
+                        <li> predaj hráča, ktorý nie je v mužstve aspon 2 týždne – <b> výrazné zhoršenie </b> (neplatí, ak je ten hráč čerstvý junior) </li>
+                        <li> predaj hráča, ktorý je v mužstve 20 týždňov a viac – <b> mierne zhoršenie </b></li>
                     </ul>
                     <br>
                     <b >Nálada </b> má tendenciu sa časom ustáliť na "normálnej" hodnote (normálna = Chladná). Náladu mužstva si viete pozrieť na <a href="https://www.hockeyarena.net/sk/index.php?p=manager_summary.php">karte</a> mužstva.                     
