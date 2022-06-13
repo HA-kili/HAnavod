33c291999fb704dde29c09efb4279f5a0f998b7f
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-06-13 15:12:07 +0200

    Add files via upload

--- menu/klucovy_hrac.php
+++ menu/klucovy_hrac.php
@@ -2,7 +2,9 @@
                 <div class="section-name"> Kľúčový hráč </div>
                     <br> 
                     Existuje možnosť ako si udržať v mužstve hráčov s nižšou lojalitou. Raz za 40 týždňov môže manažér zvoliť <b> tzv. kľúčového hráča </b> mužstva. Kľúčový hráč nepôjde automaticky na trh s nechránenými hráčmi, ale nebude možné ho ani
-                    predať ani vyhodiť. Túto možnosť nájdete tak, že kliknete na malú ikonku <img class="icons" src="./icons/plusko.gif" > pri lojalite na karte hráča. Po kliknutí na túto ikonku sa dozviete aj podrobnejšie informácie o tejto novej fukcii. 
+                    predať ani vyhodiť. Túto možnosť nájdete tak, že kliknete na malú ikonku <img class="icons" src="./icons/plusko.gif" > <u>pri lojalite </u> na karte hráča. Po kliknutí na túto ikonku sa dozviete aj podrobnejšie informácie o tejto novej fukcii. 
+                    <br><br>
+                    Po zrušení funkcie "kľúčového hráča", hráčovi pokračuje zmluva ďalej, ale jeho spokojnosť klesne na -100 % , čo môže ovplyvniť jeho ďaľšiu zmluvu - sleduj <a href="https://www.ha-navod.fun/index.php?navod=dalsie_atributy"> Spokojnosť hráča</a>. 
                     <br><br>
                     <b><h1> Kľúčového hráča je možné mať IBA v hlavnom týme. </h1></b>
                     <br><br>
