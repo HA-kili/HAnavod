ac53d005e57090f349a70e295c8f042932c1f809
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-07-11 21:03:58 +0200

    Add files via upload

---
+++ menu/zaujimavosti_manazerov.php
@@ -0,0 +1,28 @@
+
+                <div class="section-name"> Zaujímavosti manažérov </div>
+                    <br>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=manager_discussion_posts.php&table_id=531315&last_seen=12247644"><u> Pŕátel (10.07.2022) </u></a> 
+                    <br>
+                    <ul> 
+                        <li> vzniklo pretextovanie refrénu pesničky od Michala Davida, kde každý nižšie spomenutý manažér prispel svojim textom </li>
+                    </ul>
+                    <br>
+                    Pár přátel stačí mit, (<a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=696719"> janus.m|HLP </a> )
+                    <br>
+                    co uměj v pátek pít, (<a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=37083"> kili|HLP </a> )
+                    <br>
+                    a nepřestanou HA hrát, (<a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=726141"> Memed </a> )
+                    <br>
+                    když je NF začne srát... (<a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=119545"> Ovie08 </a> )
+                    <br>
+                    Tak prachy své si chraň, (<a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=716112"> jAc </a> )
+                    <br>
+                    a hrát to nepřestaň, 
+                    <br>
+                    vždyť někdy stačí říct, 
+                    <br>
+                    ten BAN mi smaž, 
+                    <br>
+                    Trsťo to dáš ... (<a href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=721117"> SpeedyG </a> )
+                    <br><br>
+                    
