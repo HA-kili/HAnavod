560d42d8162949f8822a23ce3a1e177cde212af6
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-07-19 18:42:47 +0200

    Add files via upload

--- menu/zranenia_choroby.php
+++ menu/zranenia_choroby.php
@@ -6,9 +6,17 @@
                     <br><br>
                     V prípade, že sa hráč zraní v zápase, tak nenatrénuje nič, ale získa skúsenosti. Ak sa zraní počas tréningu (na lavičke), tak 
                     tréning mu zostáva, ale skúsenosti nemá žiadne, kedže zápas nehral.                     
-                    <br><br> Zranení a chorí hráči sa zobrazujú s ikonkou malej lekárničky <img class="icons" src="./icons/lekarnicka.gif" > . 
-                    <br><br> Zranenému hráčovi sa zníži forma o 2 body za každý deň  zranenia. Podľa závažnosti zranenia to môže byť aj viac.  
-                    <br><br> Zraneným hráčom sa dĺžka zranenia mení (zmenšuje) každý deň po prepočte, čiže po 22:00 hod. 
-                    <br><br> Celková dĺžka zranenia hráča za sezónu sa zobrazuje v sekcii <a href="https://www.hockeyarena.net/sk/index.php?p=manager_team_statistics.php#x"> Menu - Hráči - Štatistiky</a> (iba pre <a href="https://www.hockeyarena.net/sk/index.php?index.php&p=public_shop.php">sponzorský balík</a> )
-                    <br><br> Zranenie hráča je možné zmenšiť pomocou dresu “Zranenie“ – sekcia <a href="index.php?navod=specialne_dresy">Dresy</a>
+                    <br><br> 
+                    Zranení a chorí hráči sa zobrazujú s ikonkou malej lekárničky <img class="icons" src="./icons/lekarnicka.gif" > . 
+                    <br><br> 
+                    Zranenému hráčovi sa zníži forma o 2 body za každý deň  zranenia. Podľa závažnosti zranenia to môže byť aj viac.  
+                    <br><br> 
+                    Zraneným hráčom sa dĺžka zranenia mení (zmenšuje) každý deň po prepočte, čiže po 22:00 hod. 
+                    <br><br> 
+                    Celková dĺžka zranenia hráča za sezónu sa zobrazuje v sekcii <a href="https://www.hockeyarena.net/sk/index.php?p=manager_team_statistics.php#x"> Menu - Hráči - Štatistiky</a> 
+                    (iba pre <a href="https://www.hockeyarena.net/sk/index.php?index.php&p=public_shop.php">sponzorský balík</a> )
+                    <br><br> 
+                    Zranenie hráča je možné zmenšiť pomocou dresu “Zranenie“ – sekcia <a href="index.php?navod=specialne_dresy">Dresy</a>
+                    <br><br>
+                    Maximálna doba zranenia hráča je 12 dní. 
                     <br><br>
