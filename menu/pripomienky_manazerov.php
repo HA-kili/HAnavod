f8d89c01b3a93c73cc28509fbaabf07e75c264b6
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-07-19 16:07:46 +0200

    Add files via upload

---
+++ menu/pripomienky_manazerov.php
@@ -0,0 +1,11 @@
+
+                <div class="section-name"> Pripomienky manažérov </div>
+                    <br><br>
+                    Tu budú spísané všetky staré i nové nápady pre zlepšenie HA pre <b> ička </b>. Či už budú aplikované do samotnej hry (novej sľubovanej verzii 6.01) 
+                    alebo nie, je už len na ňom. 
+                    <br><br>
+                    <u> Návrhy a pripomienky : </u>
+                    <br>
+                    <ul>
+                        <li> zavedenie filtru na hráčsku známku v sekcii <a href="https://www.hockeyarena.net/sk/index.php?p=manager_player_market_form.php"> Hráči, Trh</a></li>
+                    </ul>
