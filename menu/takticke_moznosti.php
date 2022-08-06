4ce3cf96588812bbf1675ffc03a2c1510f63251d
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-08-06 19:57:44 +0200

    Add files via upload

--- menu/takticke_moznosti.php
+++ menu/takticke_moznosti.php
@@ -1,6 +1,9 @@
 
                 <div class="section-name"> Taktické možnosti formácií </div>
                     <br>
+                    <h1><b>Hurá systém:</b></h1>
+                    Formácia hrá bez taktickej špecializácie, v podstate chaoticky a len podľa intuície. <b> Pri hurá systéme sa hráči vo formácii nezohrávajú! </b>
+                    <br><br>
                     <h1><b>Protiútoky:</b></h1>
                     Hráči sa budú sústrediť na dôkladnú obranu a budú čakať na chybu súpera po ktorej sa okamžite budú pokúšať o rýchly protiútok.
                     <br><br> 
@@ -16,13 +19,13 @@
                     <h1><b>Streľba od modrej:</b></h1>
                     Obrancovia budú strieľať na bránu súpera od modrej čiary.
                     <br><br> 
-                    <h1><b>Nahrávky od modrej:</b></h1>
-                    Obrancovia budú nahrávať puky pred bránu dobre postaveným útočníkom.
-                    <br><br> 
                     <h1><b>Nahrávky spoza brány:</b></h1>
                     Center sa bude snažiť rozdávať gólové prihrávky spoza brány.
-                    <br><br> <br>
+                    <br><br><br>
                     Každá z taktických špecializácií bude mať iné požiadavky na jednotlivé pozície hráčov a od toho bude závisieť jej úspešnosť. Konkrétne požiadavky na hráčske pozície pre tieto taktiky nájdete 
-                    v menu <a href="https://www.hockeyarena.net/sk/index.php?p=manager_lines_form_new.php"> Hráči - Zostava - Formácie</a>. Maximálna možná zohranosť formácie – najlepšie tri možné taktiky pre 
-                    konkrétnu formáciu už nebudú tak výrazne zvýhodnené s cieľom umožniť lepšiu použiteľnosť všetkých taktík a protitaktík.</p>
+                    v menu <a href="https://www.hockeyarena.net/sk/index.php?p=manager_lines_form_new.php"> Hráči - Zostava - Formácie</a>. 
+                    <br><br>
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=13377"> Maximálna možná zohranosť formácie</a>
+                    – najlepšie tri možné taktiky pre konkrétnu formáciu už nebudú tak výrazne zvýhodnené s cieľom umožniť lepšiu použiteľnosť všetkých taktík a protitaktík. Zrušenie miernej výhody taktiky "Hurá systém" 
+                    proti iným taktikám – táto zmena nadobudne účinnosť od 29.5.2021.  
                     <br><br>
