2b9b16852afd072ce6a5ea5996a99d2502be9615
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-08-06 15:05:41 +0200

    Add files via upload

--- menu/trening.php
+++ menu/trening.php
@@ -17,7 +17,25 @@
                     Výška atribútu závisí od kvality hráča. Totižto hráč si vie vytrénovať atribút max. do výšky 2,5* násobku kvality. Potom sa tréning daného atribútu výrazne spomalí.
                     <br><br>
                     <b> Maximálna výška každého atribútu je 255. Prípadné aktivovanie dresu, ktorý má zvýšiť atribút hráča o 10 bodov, sa tento atribút aj napriek tomu nenavýši. </b>
-                    <br><br><br><br>
+                    <br><br><br>
+                    <i><u> Kedy hráč trénuje najlepšie? </u>
+                    <br><br>
+                    Aby hráč trénoval čo najviac, potrebuje : 
+                    <br>
+                        <ul>
+                            <li> v prvom rade mať vysoký čas na ľade (icetime) - aspoň 30+ minút na zápas. Čiže okrem základnej formácie by mal hrávať aj presilovkové (PP) a oslabovkové (PK) formácie. 
+                                Treba dávať pozor aj na energiu hráča, v prípade príliš vysokého času na ľade (icetim-u), bude energia hráčovi klesať a nestihne zregenerovať na ďalší zápas. </li>
+                            <li> výška tréningu zavisí aj od <a href="https://www.ha-navod.fun/index.php?navod=zazemie"> zázemia štadióna</a>, hlavne od tréningových priestorov (čím väčšie, tým lepší tréning), 
+                                ale netreba zabúdať ani na regeneračné priestory, aby hráči stíhali regenerovať po zápasoch a mali dostatočnú energiu. </li>
+                            <li> výšku tréningu ovplyvňuje aj kvalita hráča. Čím je kvalita vyššia, tým hráč lepšie trénuje a zároveň výška kvality hráča má vplyv na dosiahnutú výšku atribútu (2,5* násobok kvality). </li>
+                            <li> výšku tréningu ovplyvňuje aj potenciál hráča. Ten rozhoduje nielen o tom, ako dlho hráč bude trénovať ale ovplyvňuje aj výšku tréningu. Čím nižši potenciál, tým nižší je tréning. </li>
+                        </ul>
+                    <br>
+                    Najlepšie tréningy hráči dosahujú v <a href="https://www.hockeyarena.net/sk/index.php?p=manager_national_wc.php"> reprezentácii</a>, potom sú to <a href="https://www.hockeyarena.net/sk/index.php?p=manager_tourney_details_form.php&tour_id=2"> Svetová liga</a>, 
+                    <a href="https://www.hockeyarena.net/sk/index.php?p=manager_tourney_details_form.php&tour_id=21"> Pohár národných šampiónov</a>, <a href="https://www.hockeyarena.net/sk/index.php?p=manager_tourney_details_form.php&inst_id=196284"> Národný pohár</a> a 
+                    aj samotná <a href="https://www.hockeyarena.net/sk/index.php?p=public_standings.inc"> liga</a>. V priateľských zápasoch už hráči dosahujú menšie tréningy.   
+                    </i>
+                    <br><br><br><br><br>
                     <i><u> A ako trénovať jednotlivé atribúty hráčov? </u>
                     <br><br>
                     Hráči sa trénujú podľa toho, aké <a href="index.php?navod=takticke_specializacie"> taktické špecializácie formácií</a> hrávate. 
