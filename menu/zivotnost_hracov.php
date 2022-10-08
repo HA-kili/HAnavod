04eaccbfa629db530eeee904b35083060ec2a5ab
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-10-09 00:39:45 +0200

    Delete zivotnost_hracov.php

--- menu/zivotnost_hracov.php
+++
@@ -1,5 +0,0 @@
-
-                <div class="section-name"> Životnosť hráčov </div>
-                    <br>
-                    Hráči vo veku 35 a viac s IŠ 8 a menej, v mužstvách <b> bez  <a href="https://www.hockeyarena.net/sk/index.php?index.php&p=public_shop.php">sponzorského balíka</a></b> , budú na konci každej sezóny automaticky poslaní <a href="https://www.hockeyarena.net/sk/index.php?p=public_ha_news.php&id=11206"> do dôchodku</a> .
-                    <br><br>
