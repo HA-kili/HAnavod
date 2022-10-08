78a32db5016fcd995582d37147ee9f5579f45b65
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-10-08 14:01:14 +0200

    Add files via upload

--- menu/doplnky_(addons).php
+++ menu/doplnky_(addons).php
@@ -1,7 +1,7 @@
 
                 <div class="section-name">Doplnky pre HA_(addons)</div>
                     <br>
-                    Manažér <a class="navod" href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=227387"> airen </a> pre Vás vytvoril doplnok - addons - 
+                    Manažér <a class="arena" href="https://www.hockeyarena.net/sk/index.php?p=manager_manager_info.php&id=227387"> airen </a> pre Vás vytvoril doplnok - addons - 
                     pre <a class="addons" href="https://addons.mozilla.org/en-US/firefox/addon/hockeyarena-extension/?utm_source=addons.mozilla.org&utm_medium=referral&utm_content=search"> mozzilu </a>
                     a 
                     pre <a class="addons" href="https://chrome.google.com/webstore/detail/hockeyarena-extension/jbcghenmgbijpebelbgiagodgfhdcnjd?hl=en"> chrome </a> . 
