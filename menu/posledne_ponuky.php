41a397381f1ef0902d66f1ad1cc0dc8ada03b3e5
HA-kili <98768793+HA-kili@users.noreply.github.com>
2022-10-09 00:39:15 +0200

    Delete posledne_ponuky.php

--- menu/posledne_ponuky.php
+++
@@ -1,6 +0,0 @@
-
-                <div class="section-name"> Zoznam posledných ponúk </div>
-                    <br>
-                    Okno, v ktorom sa zobrazia všetky odoslané ponuky na hráčov za posledných 7 dní. K jednému hráčovi sa zobrazuje iba jedna najvyššia ponuka.
-                    Tento zoznam nájdete v menu <a class="arena" href="https://www.hockeyarena.net/sk/index.php?p=manager_player_market_last_bids.php">Hráči - Trh - Zoznam posledných ponúk</a>.
-                    <br><br>
