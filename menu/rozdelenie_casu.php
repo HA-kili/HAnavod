e329bd7ee4a2a09a5136ed61f1d0c7cd97ee2313
HA-kili <98768793+HA-kili@users.noreply.github.com>
2023-03-26 11:08:31 +0200

    Delete rozdelenie_casu.php

--- menu/rozdelenie_casu.php
+++
@@ -1,7 +0,0 @@
-
-                <div class="section-name"> Rozdelenie času na ľade </div>
-                    <br>
-                    Formácie je možné posielať na ľad nerovnomerne a tým docieliť, že lepšia formácia strávi na ľade viac času, ako tá horšia. Rozdelenie času na ľade určuje presný čas v sekundách, ktorý na ľade strávia postupne všetky formácie
-                    od prvej až po poslednú. Poslednú formáciu na ľade vystrieda zasa prvá a rovnaké rozdelenie času sa použije odznova. Toto rozdelenie času viete nastaviť v menu <a class="arena" href="https://www.hockeyarena.net/sk/index.php?p=manager_tactics_form.php"> Mužstvo - Nastavenie zápasu</a>. 
-                    Čas na lade je možné zmeniť aj počas zápasu, ale o tom až v <a class="navod" href="index.php?navod=pokrocile_taktiky#icetime">pokročilejších nastaveniach</a> . 
-                    <br><br>
