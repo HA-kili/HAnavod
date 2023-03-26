ae9ea955000fdf39b940892ccee078877ba541d7
HA-kili <98768793+HA-kili@users.noreply.github.com>
2023-03-26 11:07:14 +0200

    Delete pridelenie_muzstva.php

--- menu/pridelenie_muzstva.php
+++
@@ -1,9 +0,0 @@
-
-                <div class="section-name"> Pridelenie mužstva </div>
-                    <br>
-                    <b> Prideľovanie mužstva </b> prebieha automaticky počas večerného prepočtu. Po pridelení mužstva príde manažérovi automaticky vygenerované e-mailové upozornenie. Každé pridelené mužstvo má vygenerovaných úplne nových hráčov, financie,
-                    štadión a zázemie nastavené na počiatočné hodnoty. 
-                    <br><br>
-                    Po pridelení mužstva je <b> nevyhnutné v priebehu 6 dní aktivovať </b> mužstvo a to prihlásením sa do hry. Následne je k dispozícii nastavenie názvu mužstva a neskôr v nastaveniach aj <a class="arena" href="https://www.hockeyarena.net/sk/index.php?p=manager_settings_form.php">názov štadióna</a>. 
-                    Novoregistrovaní manažéri budú dostávať pevne daných 28 hráčov bez možnosti výberu ale lepšie konkurencieschopných vo svojej lige.
-                    <br><br>
