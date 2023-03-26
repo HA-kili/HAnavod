ed3c6ba29e0f475f0fda9174c6d90b29833a5b3b
HA-kili <98768793+HA-kili@users.noreply.github.com>
2023-03-26 11:10:48 +0200

    Delete ulohy.php

--- menu/ulohy.php
+++
@@ -1,8 +0,0 @@
-
-                <div class="section-name"> Úlohy </div>
-                    <br>
-                    Pre nováčikov <u>Hockey Areny</u> je spustený systém, ktorý pomocou série jednoduchých úloh naučí manažéra základom manažovania mužstva. Za každú splnenú úlohu získa mužstvo aj malý finančný bonus. Tieto úlohy môžu samozrejme plniť aj "veteráni"
-                    a postupne budeme do tohto systému pridávať ľahké (aj ťažké) úlohy pre všetkých manažérov. 
-                    <br><br> 
-                    Úlohy nájdete v menu <a class="arena" href="https://www.hockeyarena.net/sk/index.php?p=manager_achievements.inc&action=list">Manažér - Úlohy</a>. 
-                    <br><br>
