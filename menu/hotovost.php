cb31c628ddf398d0181c547fff886a9027bb1fb1
HA-kili <98768793+HA-kili@users.noreply.github.com>
2023-03-26 11:04:33 +0200

    Delete hotovost.php

--- menu/hotovost.php
+++
@@ -1,6 +0,0 @@
-
-                <div class="section-name"> Hotovosť v hornej lište </div>
-                    <br>
-                    Hotovosť v hornej lište <img class="icons" src="./icons/hotovost.png" > bude skrytá kým si manažér nepozrie prenos z aktuálne odohraného zápasu. 
-                    Skrývanie hotovosti sa dá odteraz zapínať/vypínať v Menu <a class="arena" href="https://www.hockeyarena.net/sk/index.php?p=manager_settings_form.php">Manažér - Nastavenia</a>. 
-                    <br><br>
