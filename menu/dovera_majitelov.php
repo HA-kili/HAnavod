f43d2750fab93a28a6c683bae4f0d95c4d615c83
HA-kili <98768793+HA-kili@users.noreply.github.com>
2023-03-26 11:03:05 +0200

    Delete dovera_majitelov.php

--- menu/dovera_majitelov.php
+++
@@ -1,6 +0,0 @@
-
-                <div class="section-name"> Dôvera majiteľov </div>
-                    <br>
-                    Znamená dôveru majiteľov mužstva v schopnosti manažéra. Majitelia mužstva nedovolia uskutočniť transakcie, 
-                    ktoré by ohrozili finančnú stabilitu mužstva.
-                    <br><br>
