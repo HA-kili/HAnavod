5171ad8ebef978999a9a91db395f158f70985edf
HA-kili <98768793+HA-kili@users.noreply.github.com>
2023-03-26 11:06:09 +0200

    Delete nastavenie_zapasu.php

--- menu/nastavenie_zapasu.php
+++
@@ -1,7 +0,0 @@
-
-                <div class="section-name"> Nastavenie názvu </div>              
-                    <br>
-                    <u><i> Názov mužstva a štadióna </i></u> - Názov mužstva a štadióna je možné zmeniť 1* za sezónu. Maximálny počet znakov je 40. 
-                    <br><br>
-                    <u><i> Prenos zo zápasu </i></u> - Prenos zo zápasu je prehľadné postupné zobrazenie priebehu zápasu z predchádzajúceho dňa.
-                    <br><br>
